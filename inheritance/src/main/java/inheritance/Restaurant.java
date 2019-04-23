3f993bee4b0f231398b302d51758753027f72dd0
herndont <herndontrey@gmail.com>
2019-04-23 13:52:01 -0700

    turning in what I have completed up to this point

---
+++ inheritance/src/main/java/inheritance/Restaurant.java
@@ -0,0 +1,18 @@
+package inheritance;
+
+public class Restaurant {
+    public String name;
+    public double stars;
+    public String price;
+
+    public Restaurant(String name, double stars, String price) {
+        this.name = name;
+        this.stars = stars;
+        this.price = price;
+    }
+
+    public String toString() {
+        return "The Restaurant " + name + " has a rating of " + stars + " stars and a reference of " + price +
+                " out of 5 for price.";
+    }
+}
---
+++ inheritance/src/main/java/inheritance/Review.java
@@ -0,0 +1,19 @@
+package inheritance;
+
+public class Review {
+    public String body;
+    public String author;
+    public double stars;
+
+    public Review(String body, String author, double stars) {
+        this.body = body;
+        this.author = author;
+        this.stars = stars;
+    }
+
+    public String toString() {
+        return "Restaurant Review: " + body + " Rating Author:" + author + " Number of Stars given to the Restaurant: "
+                + stars;
+    }
+
+}
